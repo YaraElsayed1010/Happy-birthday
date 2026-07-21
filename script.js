7ebfe9d28421083a9125df04e13ab6e7a24392e5
YaraElsayed1010 <yara92671@gmail.com>
2026-07-21 22:40:35 +0300

    Create script.js
    javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("remember").onclick = function(){
+
+window.location.href = "remember.html";
+
+};
+
+document.getElementById("story").onclick = function(){
+
+window.location.href = "story.html";
+
+};
